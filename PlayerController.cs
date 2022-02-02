8dc55525f8517b0a4bf2343242114c2c4c13add8
vladyslav_mikhnevych <76562274+slavkko505@users.noreply.github.com>
2022-02-02 16:31:20 +0200

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] LayerMask movementMask;
+
+	 Camera cam;
+	 PlayerMotor motor;
+
+	 public Interaction focus;
+
+    void Start()
+    {
+        cam = Camera.main;
+		  motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+	    if (EventSystem.current.IsPointerOverGameObject())
+	    {
+		    return;
+	    }
+	    
+		 // go click
+        if(Input.GetMouseButtonDown(0))
+		  {
+			  Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			  RaycastHit hit;
+
+			  if(Physics.Raycast(ray, out hit, 100, movementMask))
+			  {
+				  motor.MoveToPointPlayer(hit.point);
+
+				  RemoveFocus();
+			  }
+		  }
+
+		// focus click
+		if (Input.GetMouseButtonDown(1))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, 100))
+			{
+				Interaction interacteble = hit.collider.GetComponent<Interaction>();
+
+				if(interacteble != null){
+					SetFocus(interacteble);
+				}
+				
+			}
+		}
+	}
+	private void SetFocus(Interaction newFocus)
+	{
+		if(newFocus != focus){
+			if(focus != null){
+				focus.OnDefocus();
+			}
+			focus = newFocus;
+			motor.FoolwTarget(newFocus);
+		}
+		newFocus.OnFocus(transform);
+	}
+	private void RemoveFocus()
+	{
+		if(focus != null){
+			focus.OnDefocus();
+		}
+		focus = null;
+		motor.StopFoolwTarget();
+	}
+}
