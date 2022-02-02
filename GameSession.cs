0a14a1b0c6235b3ce2f2949e21448fe2367a94f4
vladyslav_mikhnevych <76562274+slavkko505@users.noreply.github.com>
2022-02-02 17:35:42 +0300

    Add files via upload

---
+++ GameSession.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] float playerLifes = 3f;
+    [SerializeField] float score = 0f;
+    [SerializeField] Text livesText;
+    [SerializeField] Text scoreText;
+
+    //GameScenesIndex
+    int currentSceneIndex;
+
+    private void Awake()
+    {
+        int numGameSession = FindObjectsOfType<GameSession>().Length;
+        if(numGameSession > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Start()
+    {
+        livesText.text = playerLifes.ToString();
+        scoreText.text = score.ToString();
+    }
+    public void ProcessPlayerDeath()
+    {
+        if(playerLifes > 1)
+        {
+            TakeLife();
+        }
+        else
+        {
+            ResetGameSession();
+        }
+    }
+    public void AddToScore(float num)
+    {
+        score += num;
+        scoreText.text = score.ToString();
+    }
+    private void ResetGameSession()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene("End");
+        Destroy(gameObject);
+    }
+    private void TakeLife()
+    {
+        playerLifes--;
+        var currentGameScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentGameScene);
+        livesText.text = playerLifes.ToString();
+    }
+    public int GetCurrentSceneIndex()
+    {
+        return currentSceneIndex;
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+ 
+    public void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LoadOtionsScene()
+    {
+        SceneManager.LoadScene("Core Menu");
+    }
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene("Start Screen");
+    }
+    public void RestartGame()
+    {
+        //SceneManager.LoadScene(FindObjectOfType<Player>().GetCurrentSceneIndex());
+        Debug.Log(FindObjectOfType<GameSession>().GetCurrentSceneIndex());
+    }
+}
